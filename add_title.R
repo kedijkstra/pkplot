7d5aa740ece913aae49336b56ee43f87f419261a
lena-dehondt <lena.de.hondt@vub.be>
2026-09-08 10:31:56 +0200

    Changed title to bold and size 16

---
+++ add_title.R
@@ -0,0 +1,11 @@
+add_title <- function(p, plotTitle) {
+  p +
+    ggplot2::ggtitle(plotTitle) +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(
+        hjust = 0.5,
+        face = "bold",
+        size = 16
+      )
+    )
+}
