f8ae97603cd3d65976e4b87c0f0041760a2fd6ac
kedijkstra <keimpedijkstra22@gmail.com>
2026-09-09 11:59:37 +0200

    Added missing concentrationColumn identifier

--- R/pkmetrics.R
+++ R/pkmetrics.R
@@ -37,6 +37,7 @@ pkmetrics <- function(filePath,
       
       # Find columns
       timeColumn <- grep("^Time", names(df), value = TRUE)
+      concentrationColumn <- grep("^Concentration", names(df), value = TRUE)
       
       stopifnot(length(timeColumn) == 1) #TODO also add in other scripts
       stopifnot(length(concentrationColumn) == 1)
