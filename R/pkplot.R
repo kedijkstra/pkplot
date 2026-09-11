c572cbcc94499b6853429445e15ff2552faecdc0
lena-dehondt <lena.de.hondt@vub.be>
2026-09-11 14:57:41 +0200

    Update pkplot.R

--- R/pkplot.R
+++ R/pkplot.R
@@ -12,67 +12,93 @@ source("set_font.R")
 source("set_theme.R")
 source("log_scale.R")
 
+
+# Display plot in R
+display_plot <- function(p, displayPlot = TRUE) {
+  if (displayPlot) {
+    print(p)
+  }
+}
+
+
 pkplot <- function(
     filePath,
     outputPath,
     simType,
-    legend=TRUE,
-    greyscale=FALSE,
-    log_y=FALSE,
-    plotTitle="",
+    legend = TRUE,
+    greyscale = FALSE,
+    log_y = FALSE,
+    plotTitle = "",
     yLabel = "",
-    xLabel = ""
-    ) {
+    xLabel = "",
+    displayPlot = TRUE
+) {
   
   #Validate arguments
-  argument_validator(filePath,
-                     outputPath,
-                     simType,
-                     legend,
-                     greyscale,
-                     log_y,
-                     plotTitle,
-                     t_start=0,
-                     t_end=24)
+  argument_validator(
+    filePath,
+    outputPath,
+    simType,
+    legend,
+    greyscale,
+    log_y,
+    plotTitle,
+    t_start = 0,
+    t_end = 24
+  )
   
   #Import data
   pkData <- read_pksim(filePath = filePath)
   
   #Validate data
-    #TODO ???
+  #TODO ???
   
   #Determine plot type
   plot_specification <- determine_plot_type(pkData, simType)
   
-  #Construct individual plot
+  #Construct plot
   if (simType == "individual") {
-    p <- individual_plot(plot_specification, greyscale, yLabel, xLabel)
     
-  }else if (simType == "population") {
-    p <- population_plot(plot_specification, greyscale, yLabel, xLabel)
+    p <- individual_plot(
+      plot_specification,
+      greyscale,
+      yLabel,
+      xLabel
+    )
+    
+  } else if (simType == "population") {
+    
+    p <- population_plot(
+      plot_specification,
+      greyscale,
+      yLabel,
+      xLabel
+    )
   }
   
   #Optional log scaling
-  if (log_y){
+  if (log_y) {
     p <- log_scale(p)
   }
   
   p <- set_theme(p)
   
-  #legend layout
-  if (legend){
+  #Legend layout
+  if (legend) {
     p <- legend_layout(p)
-  } else{
+  } else {
     p <- remove_legend(p)
   }
   
-  if (length(plotTitle) > 0){
+  if (length(plotTitle) > 0) {
     p <- add_title(p, plotTitle)
   }
   
   p <- set_font(p)
-
-  print(p)
-  save_plot(outputPath, p, simType)
   
+  #Display plot
+  display_plot(p, displayPlot)
+  
+  #Save plot
+  save_plot(outputPath, p, simType)
 }
